ab4cc92f1c18a84073f78cf84400ab97ccc8ec5d
Ichigo Kurosaki <nfragale01@gmail.com>
2023-05-20 15:56:12 -0400

    Add Computer Player

--- main.js
+++ main.js
@@ -29,15 +29,19 @@ const player = (() => {
     // select symbol for player
     function selectSymbol(e) {
         player.currentPlayer = e.target.value;
-        message.textContent = "It's Player" + player.currentPlayer + "'s turn";
+        message.textContent = "It's Player " + player.currentPlayer + "'s turn";
 
         // disable drop down options after first selection
         const options = document.querySelectorAll('option');
         options[0].disabled = true;
         options[1].disabled = true;
     }
+
+    function changePlayer() {
+      player.currentPlayer = (player.currentPlayer === "X" ? "O" : "X");
+    }
     
-    return {currentPlayer, selectSymbol};
+    return {currentPlayer, selectSymbol, changePlayer};
 })()
 
 // game flow object
@@ -49,13 +53,9 @@ const game = (() => {
         // add data attribute with current players symbol value to cell
         cell.dataset.symbol = player.currentPlayer;
 
-        if(cell.dataset.symbol === 'X') {
-          cell.classList.add("x-symbol");
-        } else if (cell.dataset.symbol === 'O') {
-          cell.classList.add("o-symbol");
-        }
+        // display symbol on UI board
+        game.addSymbol(cell);
 
-        // update gameboard array
         gameBoard.updateBoard();
 
         if (game.checkForWin()) {
@@ -66,8 +66,8 @@ const game = (() => {
         } else if (game.checkForDraw()) {
           message.textContent = "It's a Tie!";
         } else {
-          player.currentPlayer = (player.currentPlayer === "X" ? "O" : "X");
-          message.textContent = "It's Player " + player.currentPlayer + "'s turn";
+          player.changePlayer();
+          game.computerMove();
         }
     }
 
@@ -106,6 +106,46 @@ const game = (() => {
         return isDraw;
     }
 
+    function computerMove() {
+
+      // find an empty index on board for the computer move
+      for(let i=0; i < gameBoard.board.length; i++) {
+  
+        const randomIndex = Math.floor(Math.random()* gameBoard.board.length);
+
+        if(gameBoard.board[randomIndex] === '') {
+          // update board
+           gameBoard.board[randomIndex] = player.currentPlayer;
+
+          //  update board UI
+           const cell = cells[randomIndex];
+           cell.dataset.symbol = player.currentPlayer;
+           game.addSymbol(cell);
+           break;
+        }
+      }
+
+      if (game.checkForWin()) {
+        message.textContent = "Computer Wins!";
+        cells.forEach(cell => {
+          cell.removeEventListener("click", handleClick);
+        });
+      } else if (game.checkForDraw()) {
+        message.textContent = "It's a Tie!";
+      } else {
+        player.changePlayer();
+        message.textContent = "It's Player " + player.currentPlayer + "'s turn";
+      }
+    }
+
+    function addSymbol(cell) {
+      if(cell.dataset.symbol === 'X') {
+         cell.classList.add("x-symbol");
+      } else if (cell.dataset.symbol === 'O') {
+        cell.classList.add("o-symbol");
+      }
+    }
+
     function resetGame() {  
         gameBoard.board = Array(9).fill('');
          
@@ -130,12 +170,13 @@ const game = (() => {
         cells.forEach(cell => cell.addEventListener('click', handleClick));
     }
 
-    return {handleClick, checkForWin, checkForDraw, resetGame}
+    return {handleClick, checkForWin, checkForDraw, computerMove, addSymbol, resetGame}
 })()
 
 document.querySelector('#reset').addEventListener('click', game.resetGame);
 select.addEventListener('change', player.selectSymbol);
 cells.forEach(cell => cell.addEventListener('click', game.handleClick));
+
 
 
 
